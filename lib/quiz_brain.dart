85b9d2888a2462849631e0054c786aa2daeb7450
fronk <fronktanaporn@gmail.com>
2022-02-11 23:24:23 +0700

    Initial commit

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,41 @@
+import 'question.dart';
+class QuizBrain {
+
+  int _questionNumber = 0;
+
+  //TODO 2: ปรับเปลี่ยนหรือเพิ่มชุดคำถามใน questionBank
+  List<Question> _questionBank=[
+    Question(q: '1 + 1 = 2', a: true),
+    Question(q: '2 + 2 = 3', a: false),
+    Question(q: '3 + 3 = 7', a: false),
+    Question(q: '7 + 7 = 14', a: true),
+    Question(q: '14 + 14 = 23', a: false),
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length-1) {
+      _questionNumber++;
+    }
+  }
+
+  String? getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool? getQuestionAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
+
+}
